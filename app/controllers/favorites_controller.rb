779bb13ac46bebee839eef8211f0116243c5cec5
Raghu Betina <raghu@firstdraft.com>
2018-01-08 23:07:23 +0000

    Only favorite user can modify favorite

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,4 +1,14 @@
 class FavoritesController < ApplicationController
+  before_action :current_user_must_be_favorite_user, :only => [:edit, :update, :destroy]
+
+  def current_user_must_be_favorite_user
+    favorite = Favorite.find(params[:id])
+
+    unless current_user == favorite.user
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @favorites = Favorite.all
 
