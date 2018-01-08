8b82535b26af302cf6a2f3c74ea467960497b184
Raghu Betina <raghu@firstdraft.com>
2018-01-08 23:07:13 +0000

    Whitelist columns in Favorite for ActiveAdmin

--- app/admin/favorite.rb
+++ app/admin/favorite.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Favorite do
 
+ permit_params :user_id, :dishes_id, :venue_id, :cuisine_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
